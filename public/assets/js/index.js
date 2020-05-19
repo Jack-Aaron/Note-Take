e25ff34f020a4e0a55baae8f1bf6e37b73484015
Jonathan Alpart <panjialang@gmail.com>
2020-05-19 17:37:48 -0500

    added PORT, requires and express

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,6 +4,22 @@ const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
 
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use
+
+
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
