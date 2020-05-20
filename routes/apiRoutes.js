c40fa3984ae632e1804b61276bae8ecc4a573c84
Jonathan Alpart <panjialang@gmail.com>
2020-05-20 17:09:00 -0500

    notesData is not a function?

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,61 +4,60 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-const notesData = require("../db/notesData");
+var notesData = require("../db/notesData");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function (req, res) {
-    res.json(notesData);
-  });
+    app.get("/api/notes", function (req, res) {
+        res.json(notesData);
+    });
 
-//   app.get("/api/waitlist", function (req, res) {
-//     res.json(waitListData);
-//   });
+    //   app.get("/api/waitlist", function (req, res) {
+    //     res.json(waitListData);
+    //   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function (req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (notesData.length < 5) {
-        notesData.push(req.body);
+    app.post("/api/notes", function (req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+            notesData.push(req.body);
 
-      //these lines break the alerts
-      res.json(true);
-    // } else {
-    //   waitListData.push(req.body);
-    //   console.log(req.body);
-    //   //these lines break the alerts
-    //   res.json(false);
-    // }
-  });
+            //these lines break the alerts
+            res.json(true);
+        // else {
+        //   waitListData.push(req.body);
+        //   console.log(req.body);
+        //   //these lines break the alerts
+        //   res.json(false);
+        // }
+    })
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
 
-  app.delete("/api/clear", function (req, res) {
-    // Empty out the arrays of data
-    notesData;
-    // waitListData;
+    app.delete("/api/clear", function (req, res) {
+        // Empty out the arrays of data
+        notesData;
+        // waitListData;
 
-    res.json({ ok: true });
-  });
+        res.json({ ok: true });
+    });
 };
