1f394d25f9113c4a9a4b711e4f8966a12805ee0d
Jonathan Alpart <panjialang@gmail.com>
2020-05-20 13:39:22 -0500

    added routes

--- server.js
+++ server.js
@@ -6,24 +6,30 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/assets/index.html"));
-});
-
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/assets/notes.html"));
-});
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.set('port', process.env.PORT || 3000);
+
+//static for Heroku
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+
+require('./routes/apiRoutes')(app);
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
